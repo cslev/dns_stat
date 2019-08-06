39a8cfc36ca9b0e7bc690ff54cf552ecea199adc
cslev <cslev@gmx.com>
2019-08-06 14:32:16 +0800

    Fix errors

--- dns_stat.sh
+++ dns_stat.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 source colorized_output.sh
 
@@ -14,9 +14,16 @@ function print_help ()
   c_print yellow "Usage:"
   c_print bold "\t./dns_stat.sh [filename_of_domains] [only_one_ns_at_a_time] [output]"
   c_print none "\t-> filename_of_domains: list of domains each in a separate line in the file"
-  c_print none "\t-> only_one_ns_at_a_time: 0/1 (there can be even more than 200 nameservers for a domain which clearly lengthens the running time) - 0 means only one NS, 1 means all of them"
+  c_print none "\t-> only_one_ns_at_a_time: 0/1 (there can be even more than 200 nameservers for a domain which clearly lengthens the running time) - 1 means only one NS, 0 means all of them"
   c_print none "\t-> output: the output filename where the results will be saved"
   exit -1
+}
+
+function calculate_rtt ()
+{
+  ip=$1
+  res=$(ping $ip -c 3 -q |grep "avg"|cut -d '=' -f 2|sed 's/ //g'|cut -d '/' -f 2)
+  return $res
 }
 
 if [ $# -ne 3 ]
@@ -34,54 +41,57 @@ then
   print_help
 fi
 
-if [ $only_one_ns -eq 0 ]
-then
-  cmd = "dig $domain_name NS +tries=1 +time=2 |grep -v "AAAA"|grep A|grep -v ";"|awk '{print $1 "-" $5}'"
-else
-  cmd = "dig $domain_name NS +tries=1 +time=2 |grep -v "AAAA"|grep A|grep -v ";"|head -n1| awk '{print $1 "-" $5}'"
-fi
+output="${output}.csv"
 
 c_print bold "Analysing $filename for domains:"
 c_print bold "\tOnly one ns = $only_one_ns"
 c_print bold "\tOutput file will be: ${output}"
 
+
 echo "ID,IP,NAMESERVER,Ping RTT,Query time,Msg size" > $output
 
-function calculate_rtt ()
-{
-  ip=$1
-  res=$(ping $ip -c 3 -q |grep "avg"|cut -d '=' -f 2|sed 's/ //g'|cut -d '/' -f 2)
-  return $res
-}
 
 ID=1
 
 for domain_name in $(cat $filename|cut -d ',' -f 2)
 do
+  cmd="dig $domain_name NS |grep NS|grep -v ';'| sed 's/[[:blank:]]/;/g'|cut -d ';' -f 6"
+  # if we don't care about all nameservers, we simply add head -n1 to the command
   c_print blue "Checking ${domain_name}..."
   sleep 1
   #go through the NS records only and filter out AAAA (IPv6 records) and get the
   #nameserver's domain name and its corresponding IP address (hence awk $1, $5)
-  for i in $($cmd)
-  do
-    #get the nameserver
-    ns=$(echo $i|cut -d '-' -f 1)
-    #there is a dot (.) at the end of it...get rid of that
-    ns=$(echo "${ns%?}")
-    # get the IP address assigned to the DNS
-    ip=$(echo $i|cut -d '-' -f 2)
+  # echo $cmd
+  for i in $(dig $domain_name NS |grep NS|grep -v ';'| sed 's/[[:blank:]]/;/g'|cut -d ';' -f 6)
+    do
+      # echo $i
+      #there is a dot (.) at the end of the nameserver...get rid of that
+      ns=$(echo "${i%?}")
 
-    #print out intermediate information
-    c_print yellow "Querying ${ns} (at ${ip}) for the ANY records of ${ns} itself!"
-    dig @$ip $ns ANY +tries=1 +time=2|tail -n 5 > tmp_${ns}
-    query_time=$(cat tmp_${ns}| grep -i "Query time"| cut -d ':' -f 2|sed 's/ //g')
-    msg_size=$(cat tmp_${ns}| grep -i "MSG SIZE"| cut -d ':' -f 2|sed 's/ //g')
+      # get the IP address assigned to the DNS
+      ip=$(nslookup $ns |grep Address|tail -n1|cut -d ':' -f 2|sed 's/ //g')
 
-    calculate_rtt $ip
-    ping_rtt=$(echo $?)
+      #print out intermediate information
+      c_print yellow "Querying ${ns} (at ${ip}) for the ANY records of ${ns} itself!"
+      # echo "dig @$ip $ns ANY +tries=1 +time=2|tail -n 5 > tmp_${ns}"
+      dig @$ip $ns ANY +tries=1 +time=2|tail -n 5 > tmp_${ns}
+      query_time=$(cat tmp_${ns}| grep -i "Query time"| cut -d ':' -f 2|sed 's/ //g')
+      msg_size=$(cat tmp_${ns}| grep -i "MSG SIZE"| cut -d ':' -f 2|sed 's/ //g')
 
-    echo "${ID},${ip},${ns},${ping_rtt},${query_time},${msg_size}" >> dns_stats.csv
-    rm -rf tmp_${ns}
-    ID=`expr $ID + 1`
-  done
+      ping_rtt=$(ping $ip -c 3 -q |grep "avg"|cut -d '=' -f 2|sed 's/ //g'|cut -d '/' -f 2)
+
+      echo "${ID},${ip},${ns},${ping_rtt},${query_time},${msg_size}" >> $output
+      rm -rf tmp_${ns}
+      ID=`expr $ID + 1`
+
+      #we break the loop if only the first NS should have been considered only
+      if [ $only_one_ns -eq 1 ]
+      then
+        break
+       #there was no other solution for this! Adding |head -n1 to the command
+       #seems a good idea but then the comman should be stored as a variable.
+       #which is cumbersome to execute efficiently (e.g., via $($command) does
+       #not work all the time)
+      fi
+    done
 done
